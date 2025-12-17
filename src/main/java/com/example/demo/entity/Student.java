7f0c0ed9eab5b9888ae28e753365c2e3fd3ca5c3
bala-amypo <bala@amypo.in>
2025-12-17 06:29:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -5,5 +5,25 @@ public class Student{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
 
-    pri
+    private long int id;
+    private String name;
+    private String email;
+    public Student(long int id,String name,String email,LocalDate dob,float cgpa){
+        this.id=id;
+        this.naem=name;
+        this.email=email;
+
+    }
+    public String getName(){
+        retrun name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
 }
