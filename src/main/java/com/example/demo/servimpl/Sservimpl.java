2bc1815c29992cd0a36b6ecee4cf039218b3726f
bala-amypo <bala@amypo.in>
2025-12-17 09:24:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/servimpl/Sservimpl.java
@@ -0,0 +1,11 @@
+package com.example.dem.Servimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.dem.service.StudentService;
+
+@Service
+public class Sservimpl implements StudentService{
+    
+
+}
