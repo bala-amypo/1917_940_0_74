c843f549bde12a349ec0c9817e4e788c9963f570
bala-amypo <bala@amypo.in>
2025-12-17 09:49:49 +0000

    User commit

--- src/main/java/com/example/demo/servimpl/Sservimpl.java
+++ src/main/java/com/example/demo/servimpl/Sservimpl.java
@@ -1,11 +1,25 @@
-package com.example.dem.Servimpl;
+package com.example.demo.servimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.dem.model.StudentDetail;
+import com.example.dem.repository.StudentRepository;
 import com.example.dem.service.StudentService;
 
 @Service
 public class Sservimpl implements StudentService{
+    // private final StudentRepository strep;
+    // public Sservimpl(StudentRepository strep){
+    //     this.strep=strep;
+    // }
+
+    @Autowired
+    StudentRepository strep;
+
+    public Student saveStudent(Student st){
+        return strep.save(student);
+    }
     
 
 }
