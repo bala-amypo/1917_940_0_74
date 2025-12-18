8ef24de55e3dd8891e1d97359e593de354907198
bala-amypo <bala@amypo.in>
2025-12-18 10:31:24 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,13 @@
- package com.example.demo.serviceimpl;
+package com.example.demo;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-    import com.example.demo.entity.Student;
-    import com.example.demo.repository.StudentRepository;
-    import com.example.demo.service.StudentService;
-    import com.example.demo.exception.ResourceNotFoundException;
-    @Service
-    public class StudentServiceImple implements StudentService {
-        @Autowired
-        StudentRepository studentRepository;
+@SpringBootApplication
+public class DemoApplication {
 
-        public Student saveStudent(Student student){
-            return studentRepository.save(student);
-        }
-        public Student getStudentById(Long id){
-            return studentRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Student not found"));
-}}
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
--- src/main/java/com/example/demo/servimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/servimpl/StudentServiceImple.java
@@ -1,19 +1,20 @@
-package com.example.demo.serviceimpl;
+  package com.example.demo.serviceimpl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Student;
-import com.example.demo.repository.StudentRepository;
-import com.example.demo.service.StudentService;
+    import com.example.demo.entity.Student;
+    import com.example.demo.repository.StudentRepository;
+    import com.example.demo.service.StudentService;
+    import com.example.demo.exception.ResourceNotFoundException;
+    @Service
+    public class StudentServiceImple implements StudentService {
+        @Autowired
+        StudentRepository studentRepository;
 
-@Service
-public class StudentServiceImple implements StudentService {
-    @Autowired
-    StudentRepository studentRepository;
-
-    public Student saveStudent(Student student){
-        return studentRepository.save(student);
-    }
-    
-}
+        public Student saveStudent(Student student){
+            return studentRepository.save(student);
+        }
+        public Student getStudentById(Long id){
+            return studentRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+}}
